be4f8c8395b750f70ebec111f217656273e0a175
DaichiNakagama <nakadaichi@icloud.com>
2020-10-11 22:45:49 +0900

    sa

--- HideMonoChrome/Assets/Script/Object/StageGrider.cs
+++ HideMonoChrome/Assets/Script/Object/StageGrider.cs
@@ -1,5 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Rendering;
+
+enum BlockType
+{
+    Black,
+    White,
+    Gray,
+    Warp,
+    Max
+}
 
 public class Array
 {
@@ -29,12 +40,16 @@ public class StageGrider : MonoBehaviour
     private Vector2Int _cellnum;         // 升目の数
     float _offset = 0.5f;
     private Array _array;               // ステージの縦横サイズ
-    private int[,] _blockType;         // ブロックの種類
+    private BlockType[,] _blockType;    // ブロックの種類
     private GameObject _blocks;         // 全ブロックの親オブジェクト
     [SerializeField]
     private GameObject _black;          // 黒ブロック
     [SerializeField]
     private GameObject _white;          // 白ブロック
+    [SerializeField]
+    private GameObject _gray;           // 灰ブロック
+    Dictionary<BlockType, GameObject> 
+        _blockTable;                    // ブロックテーブル
 
     private GameObject _player;         // プレイヤーの情報
     private GameObject _enemyParent;    // 敵キャラクターの親オブジェクト
@@ -48,13 +63,23 @@ public class StageGrider : MonoBehaviour
         _array = new Array(_cellnum);
         _blocks = new GameObject("_blocks");
         _blocks.transform.parent = transform;
-        _blockType = new int[_cellnum.x, _cellnum.y];
+        _blockType = new BlockType[_cellnum.x, _cellnum.y];
+        CreateBlockTable();
 
         _enemyParent = GameObject.Find("Enemy");
         _player = GameObject.Find("player");
         gameObject.AddComponent<SortingGroup>().sortingLayerName = "object";
         InitBlocks();
         SetImage();
+    }
+
+    private void CreateBlockTable()
+    {
+        _blockTable = new Dictionary<BlockType, GameObject>();
+        _blockTable.Add(BlockType.Black, _black);
+        _blockTable.Add(BlockType.White, _white);
+        _blockTable.Add(BlockType.Gray, _gray);
+        // _blockTable.Add(BlockType.Warp, _gray);
     }
 
     // 初期のブロック配置を決定
@@ -64,36 +89,33 @@ public class StageGrider : MonoBehaviour
         {
             for (int i = 0; i < _cellnum.y; i++)
             {
-                var r = (int)Random.Range(0, 100f) % 2;
-                _blockType[p, i] = r;
+                _blockType[p, i] = (BlockType)(UnityEngine.Random.Range(0, 100f) % 2);
             }
         }
-        var x = (int)Random.Range(1, _cellnum.x);
-        var y = (int)Random.Range(1, _cellnum.y);
+        var x = (int)UnityEngine.Random.Range(1, _cellnum.x);
+        var y = (int)UnityEngine.Random.Range(1, _cellnum.y);
         _keyPos = new Vector3(x + _offset, y + _offset);
+        _blockType[x, y] = BlockType.Black;
         _enemyParent.transform.GetChild(0).transform.position = _keyPos;
-        _blockType[x,y] = 1;
-        _blockType[0, 0] = 1;
+        for (int i = 0; i < 10; i++)
+        {
+            x = (int)UnityEngine.Random.Range(1, _cellnum.x);
+            y = (int)UnityEngine.Random.Range(1, _cellnum.y);
+            _blockType[x, y] = BlockType.Gray;
+        }
+        _blockType[0, 0] = BlockType.Black;
     }
 
     void SetImage()
-    {      
+    {
         // 子オブジェクトの登録
         for (int p = 0; p < _cellnum.x; p++)
         {
             for (int i = 0; i < _cellnum.y; i++)
             {
                 Vector3 pos = new Vector3(p + _offset, i + _offset);
-                if (_blockType[p,i] == 0)
-                {
-                    GameObject obj = (GameObject)Instantiate(_white, pos, Quaternion.identity);
-                    obj.transform.parent = _blocks.transform;              
-                }
-                else
-                {
-                    GameObject obj = (GameObject)Instantiate(_black, pos, Quaternion.identity);
-                    obj.transform.parent = _blocks.transform;
-                }
+                GameObject obj = (GameObject)Instantiate(_blockTable[_blockType[p, i]], pos, Quaternion.identity);
+                obj.transform.parent = _blocks.transform;             
             }
         }
     }
@@ -123,7 +145,18 @@ public class StageGrider : MonoBehaviour
                 if (!((p == pos.x && i == pos.y)
                  || (p == key.x && i == key.y)))
                 {
-                    _blockType[p, i] = (tmp[p,i] - 1) * -1;
+                    if(tmp[p,i] == BlockType.Black)
+                    {
+                        _blockType[p, i] = BlockType.White;
+                    }
+                    else if(tmp[p, i] == BlockType.White)
+                    {
+                        _blockType[p, i] = BlockType.Black;
+                    }
+                    else
+                    {
+                        _blockType[p, i] = tmp[p, i];
+                    }
                 }
             }
         }
