3f08d3eb1ab8ddde0d77885f2e0de85aa724bb02
DaichiNakagama <nakadaichi@icloud.com>
2020-10-13 00:54:21 +0900

    12

--- HideMonoChrome/Assets/Script/Chara/Warp.cs
+++ HideMonoChrome/Assets/Script/Chara/Warp.cs
@@ -7,6 +7,19 @@ public class Warp : MonoBehaviour
     [SerializeField]
     private GameObject _warp;
     float _start;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        int i = 0;
+        i = 1;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        _start = 0f;
+    }
     private void OnTriggerExit2D(Collider2D col)
     {
         _start = 0f;
